96fda1ba8bdb4c9dd28a5a91958768ad6a131050
jungdongeui <jde8993@naver.com>
2017-12-21 13:47:24 +0900

    storage change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import {UploadParams } from 'angular-azure-blob-service';
 
 export const Config: UploadParams = {
-    sas: '?sv=2017-04-17&ss=bfqt&srt=sco&sp=rwdlacup&se=2017-12-31T23:59:59Z&st=2017-12-01T10:01:03Z&sip=192.168.15.2&spr=https,http&sig=Zkt%2Bkyltjdc7dda5gCb6mMvRFgE7Suk7jjHuePtkmjc%3D',
-    storageAccount: 'storageaccount1205',
-    containerName: 'imageblobcontainer'
+    sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2017-12-23T12:45:53Z&st=2017-12-20T04:45:53Z&sip=192.168.15.2&spr=https,http&sig=tVMb%2BoynWf19dMS3TKJIUrRfC33ZODHuNT%2F%2FvW5Zspk%3D',
+    storageAccount: 'dongeuistorage',
+    containerName: 'blob'
     
   };
