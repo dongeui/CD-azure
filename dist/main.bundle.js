0ad2cc4974790b45616844176ff7068d8d2dff2b
jungdongeui <jde8993@naver.com>
2017-12-22 15:31:24 +0900

    change sas time

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -115,7 +115,7 @@ var AppComponent = (function () {
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Config; });
 var Config = {
-    sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2017-12-23T12:45:53Z&st=2017-12-20T04:45:53Z&sip=192.168.15.2&spr=https,http&sig=tVMb%2BoynWf19dMS3TKJIUrRfC33ZODHuNT%2F%2FvW5Zspk%3D',
+    sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2017-12-22T14:30:13Z&st=2017-12-22T06:10:13Z&sip=192.168.15.2&spr=https,http&sig=JJyk2XtC969ggrL85ahop8f7JnwH%2Br9g9adq0ML8BlE%3D',
     storageAccount: 'dongeuistorage',
     containerName: 'blob'
 };
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import {UploadParams } from 'angular-azure-blob-service';
 
 export const Config: UploadParams = {
-    sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2017-12-23T12:45:53Z&st=2017-12-20T04:45:53Z&sip=192.168.15.2&spr=https,http&sig=tVMb%2BoynWf19dMS3TKJIUrRfC33ZODHuNT%2F%2FvW5Zspk%3D',
+    sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2017-12-22T14:30:13Z&st=2017-12-22T06:10:13Z&sip=192.168.15.2&spr=https,http&sig=JJyk2XtC969ggrL85ahop8f7JnwH%2Br9g9adq0ML8BlE%3D',
     storageAccount: 'dongeuistorage',
     containerName: 'blob'
     
