aae604f2130245331c8805ee781f9c63a897de55
jungdongeui <jde8993@naver.com>
2017-12-26 09:11:50 +0900

    sas 수정

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import {UploadParams } from 'angular-azure-blob-service';
 
 export const Config: UploadParams = {
-    sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2017-12-22T14:30:13Z&st=2017-12-22T06:10:13Z&sip=192.168.15.2&spr=https,http&sig=JJyk2XtC969ggrL85ahop8f7JnwH%2Br9g9adq0ML8BlE%3D',
+    sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2017-12-26T23:59:59Z&st=2017-12-26T01:00:00Z&sip=192.168.15.1-192.168.15.10&spr=https,http&sig=yqRoBBu90ei4v7yGFTrmodi6B9wIUAJHGAfLyzNysL8%3D',
     storageAccount: 'dongeuistorage',
     containerName: 'blob'
     
