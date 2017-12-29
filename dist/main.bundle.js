5e022a0f6d487f754041099e7859b244b40e906b
jungdongeui <jde8993@naver.com>
2017-12-29 14:44:53 +0900

    sasssssssss

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -116,7 +116,7 @@ var AppComponent = (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Config; });
 var Config = {
     sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2018-01-02T23:16:38Z&st=2017-12-26T15:16:38Z&sip=192.168.15.2&spr=https,http&sig=8ZuoMdrrGBcs0HA74S7z8K04Kw3ZHIsl84bBPv30vDU%3D',
-    storageAccount: 'angularblob',
+    storageAccount: 'angularblob2',
     containerName: 'blob'
 };
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import {UploadParams } from 'angular-azure-blob-service';
 
 export const Config: UploadParams = {
     sas: '?sv=2017-04-17&ss=b&srt=sco&sp=rwdlac&se=2018-01-02T23:16:38Z&st=2017-12-26T15:16:38Z&sip=192.168.15.2&spr=https,http&sig=8ZuoMdrrGBcs0HA74S7z8K04Kw3ZHIsl84bBPv30vDU%3D',
-    storageAccount: 'angularblob',
+    storageAccount: 'angularblob2',
     containerName: 'blob'
     
   };
